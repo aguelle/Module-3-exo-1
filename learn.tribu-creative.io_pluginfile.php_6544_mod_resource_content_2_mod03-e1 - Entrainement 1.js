287abd4e81a97ede7d81c16dcf362a8f2a561c8c
Aurélien Guelle <aurelien.guelle@gmail.com>
2023-08-23 16:51:57 +0200

    Module 3, exo1, questions 1 et 2 ok

---
+++ learn.tribu-creative.io_pluginfile.php_6544_mod_resource_content_2_mod03-e1 - Entrainement 1.js
@@ -0,0 +1,130 @@
+// Pour chaque question, implémentez la fonction demandée.
+// Pour chaque fonction, faire au moins 2 appels de la fonction avec des paramètres différents et afficher les résultats dans la console.
+
+/* ------------------------------------------------------*/
+
+console.log(
+  "1/ Implémentez une fonction qui retourne la somme de ses 2 paramètres."
+);
+
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(10, 1));
+
+/* ------------------------------------------------------*/
+
+console.log(
+  "2/ Implémentez une fonction qui retourne la plus petite des 2 valeurs passées en paramètre."
+);
+
+function smal(a, b) {
+  if (a > b) {
+    return b;
+  }
+  return a;
+}
+console.log(smal(5, 66));
+
+/* ------------------------------------------------------*/
+
+console.log(
+  "3/ Implémentez une fonction qui retourne la dernière valeur d'un tableau."
+);
+
+let table = [1, 2, 3, 4];
+
+function lastValueOfTable(value) {
+  return;
+}
+console.log(lastValueOfTable(table));
+
+/* ------------------------------------------------------*/
+
+console.log(
+  "4/ Implémentez une fonction qui retourne la valeur en entrée augmentée d'un pourcentage d'augmentation."
+);
+
+console.log();
+console.log();
+
+/* ------------------------------------------------------*/
+
+console.log(
+  "5/ Implémentez une fonction qui retourne la valeur maximale entre 3 valeurs."
+);
+
+console.log();
+console.log();
+
+/* ------------------------------------------------------*/
+
+console.log(
+  "6/ Implémentez une fonction qui retourne la somme des valeurs d'un tableau."
+);
+
+console.log();
+console.log();
+
+/* ------------------------------------------------------*/
+
+console.log(
+  "7/ Implémentez une fonction qui retourne une valeur aléatoire entre 0 et un nombre."
+);
+
+console.log();
+console.log();
+
+/* ------------------------------------------------------*/
+
+console.log(
+  "8/ Implémentez une fonction qui retourne la moyenne des valeurs d'un tableau."
+);
+
+console.log();
+console.log();
+
+/* ------------------------------------------------------*/
+
+console.log(
+  "9/ Implémentez une fonction qui retourne une durée en heures et minutes depuis une durée en minutes."
+);
+
+console.log();
+console.log();
+
+/* ------------------------------------------------------*/
+
+console.log(
+  "10/ Implémentez une fonction qui retourne la valeur minimale d'un tableau."
+);
+
+console.log();
+console.log();
+
+/* ------------------------------------------------------*/
+
+console.log(
+  "11/ Implémentez une fonction qui retourne la clé d'un objet pour la valeur maximale ."
+);
+
+console.log();
+console.log();
+
+/* ------------------------------------------------------*/
+
+console.log(
+  "12/ Implémentez une fonction qui retourne une valeur aléatoire dans un tableau."
+);
+
+console.log();
+console.log();
+
+/* ------------------------------------------------------*/
+
+console.log(
+  "13/ Implémentez une fonction qui retourne le nombre de mots dans un texte."
+);
+
+console.log();
+console.log();
